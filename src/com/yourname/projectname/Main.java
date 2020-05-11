8961ca9adcd97719343f957a7c22ec41f55c1c34
duplxey <nick.tomazic@gmail.com>
2020-05-11 11:17:45 +0200

    Full reorganization.

--- src/com/yourname/projectname/Main.java
+++
@@ -1,8 +0,0 @@
-package com.yourname.projectname;
-
-public class Main {
-
-    public static void main(String[] args) {
-        // This doesn't do anything. Change Main to one of the example's Main.
-    }
-}
